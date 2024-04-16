1b543ef168ca6ae64209745236a4c28bea71ff0f
João Otávio Leites Matuszevski <joaoolmdev@gmail.com>
2024-04-16 11:08:40 -0300

    passoword Changed

--- app/settings.py
+++ app/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'carros',
         'USER': 'postgres',
-        'PASSWORD': '0254',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
